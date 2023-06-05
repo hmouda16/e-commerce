9eeff3e39dfaa2bf9efd3acec06b6cb5286f6959
ahmed <ahmed-1611@live.fr>
2023-06-05 11:06:00 +0200

    second commit

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-    <footer class="d-flex flex-row justify-content-center align-items-center">
-        <p>Copyright © 2023 Ahmed</p>
-    </footer>
+  <footer class="d-flex flex-row justify-content-center align-items-center">
+    <p>Copyright © 2023 Ahmed</p>
+  </footer>
 </template>
-    
-  
 
 <style lang="scss" scoped>
-    footer{
-        background-color: var(--gray-3);
-        color: var(--text-primary-color);
-    }
+footer {
+  background-color: var(--gray-3);
+  color: var(--text-primary-color);
+}
 </style>
-
